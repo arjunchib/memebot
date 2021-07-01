6b65d59ddcb88e172657b59ae549977e38aeeb46
Arjun Chib <arjunpatrickchib@gmail.com>
2021-07-01 00:56:59 -0400

    Add disky

--- diskyrc.js
+++ diskyrc.js
@@ -1,4 +1,5 @@
 export default {
   prefix: "!",
   src: "./src",
+  token: process.env.BOT_TOKEN,
 };
---
+++ src/commands/list.ts
@@ -0,0 +1,5 @@
+export default {
+  command({ msg }) {
+    msg.reply("Visit memebot.life for a list of memes.");
+  },
+};
