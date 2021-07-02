b2a14b5c4a6530346967f81463f57927f89ff08c
Arjun Chib <arjunpatrickchib@gmail.com>
2021-07-02 02:19:25 -0400

    Update to use disky command

--- src/commands/_default.ts
+++ src/commands/_default.ts
@@ -1,16 +1,25 @@
+import { Command } from "disky";
 import axios from "axios";
 
-export default {
+export default new Command({
   async command({ msg }) {
     if (msg.member.voice.channel) {
       const name = msg.content.slice(1);
-      const [meme, connection] = await Promise.all([
-        axios.get(`${process.env.MEME_ARCHIVE_BASE_URL}/commands/${name}.json`),
-        msg.member.voice.channel.join(),
-      ]);
-      connection.play(meme.data.audio);
+      try {
+        const meme = await axios.get(
+          `${process.env.MEME_ARCHIVE_BASE_URL}/commands/${name}.json`
+        );
+        const connection = await msg.member.voice.channel.join();
+        const dispatcher = connection.play(meme.data.audio);
+        dispatcher.on("finish", () => {
+          connection.disconnect();
+        });
+        dispatcher.on("error", console.error);
+      } catch (e) {
+        console.error(e);
+      }
     } else {
       msg.reply("You need to join a voice channel first!");
     }
   },
-};
+});
--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,5 +1,7 @@
-export default {
+import { Command } from "disky";
+
+export default new Command({
   command({ msg }) {
     msg.reply("Visit memebot.life for a list of memes.");
   },
-};
+});
