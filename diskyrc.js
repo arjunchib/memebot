0095f9ccb4172c2b29c5e021bf501adc3cbc46ac
Arjun Chib <arjunpatrickchib@gmail.com>
2021-07-19 20:43:09 -0400

    Update config

---
+++ diskyrc.js
@@ -0,0 +1,7 @@
+const options = {
+  prefix: process.env.PREFIX,
+  token: process.env.BOT_TOKEN,
+  intents: ["GUILD_VOICE_STATES"],
+};
+
+export default options;
--- diskyrc.ts
+++
@@ -1,9 +0,0 @@
-import { ClientOptions } from "disky";
-
-const options: ClientOptions = {
-  prefix: process.env.PREFIX,
-  token: process.env.BOT_TOKEN,
-  intents: ["GUILD_VOICE_STATES"],
-};
-
-export default ClientOptions;
