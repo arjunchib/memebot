8140bc38c7487c95f09e9f7b5a339693fb376f9e
Arjun Chib <arjunpatrickchib@gmail.com>
2021-07-21 17:34:53 -0400

    Update search to search commands

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -1,7 +1,7 @@
 import { Command, CommandContext, meta } from "disky";
 import axios from "axios";
 
-interface Meme {
+interface CommandData {
   name: string;
 }
 
@@ -16,14 +16,14 @@ export default class SearchCommand implements Command {
       process.env.PREFIX.length + "search ".length
     );
     const res = await axios.get(
-      `${process.env.MEME_ARCHIVE_BASE_URL}/memes.json?s=${searchStr}`
+      `${process.env.MEME_ARCHIVE_BASE_URL}/commands.json?s=${searchStr}`
     );
     await msg.channel.send(this.resultText(res.data, searchStr));
   }
 
-  resultText(memes: Meme[], searchStr: string) {
-    return memes
-      .map((meme) => meme.name)
+  resultText(commands: CommandData[], searchStr: string) {
+    return commands
+      .map((command) => command.name)
       .join(", ")
       .replace(RegExp(searchStr, "gi"), `**$&**`);
   }
